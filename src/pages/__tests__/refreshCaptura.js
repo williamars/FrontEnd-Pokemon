55e4330b6b8df9efeb35e4ca2611c03529ca522f
Henrique Thome <henrique.thome@hotmail.com>
2020-11-28 18:21:37 -0300

    refresh de pokemons para capturar e respectivo teste

---
+++ src/pages/__tests__/refreshCaptura.js
@@ -0,0 +1,15 @@
+import React from "react"
+import DeleteAccount from "../store"
+import { render, fireEvent, cleanup } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+
+// Reseta o render do component após cada teste
+afterEach(cleanup)
+it("Quando deletar, deve aparecer o carregamento após o click", () => {
+  const { getByTestId, getByText } = render(<DeleteAccount />)
+
+  // Clica no botão de dar refresh
+  const refreshPage = getByText(/reload!/i)
+  expect(refreshPage).toBeInTheDocument()
+  refreshPage.click()
+})
