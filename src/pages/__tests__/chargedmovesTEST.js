48eb87931fda54aa67efe322ddd870c6861572f2
GustavovPierre <38561106+GustavovPierre@users.noreply.github.com>
2020-11-29 17:42:44 -0300

    funcionalidade e teste

---
+++ src/pages/__tests__/chargedmovesTEST.js
@@ -0,0 +1,27 @@
+import React from "react"
+import MovesPage from "../chargedmoves"
+import { mount, configure } from "enzyme"
+import toJson from "enzyme-to-json"
+import Adapter from "enzyme-adapter-react-16"
+
+
+afterEach(cleanup)
+
+describe("charged moves test", () => {
+    const props = {
+        location: {
+          state: {
+            types: [{type: 'grass', checked: true}, {type: 'fire', checked: true}, {type: 'water', checked: true}, {type: 'bug', checked: true}, {type: 'normal', checked: true}, {type: 'dark', checked: true}, {type: 'poison', checked: true}, {type: 'electric', checked: true}, {type: 'ice', checked: true}, {type: 'ground', checked: true}, {type: 'fairy', checked: true}, {type: 'steel', checked: true}, {type: 'fighting', checked: true}, {type: 'psychic', checked: true}, {type: 'rock', checked: true}, {type: 'ghost', checked: true}, {type: 'dragon', checked: true}, ],
+            search: '',
+            lista: [{name: 'Test', type:'Normal', power:60, energy_delta:-33,critical_chance: 0.05},{name: 'Debug', type:'Dark', power:150, energy_delta:-50,critical_chance: 0.15}]
+          },
+        },
+      }
+
+    });
+
+    const app = mount(<MovesPage {...props} />)
+    
+    
+    it("should check the list", () => {
+     expect(toJson(app)).toMatchSnapshot() });
